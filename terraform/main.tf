decd1e28ea3250ba4b2f25bb2f17db4c3a85a8c7
JoseLandivar <joseantoniolandivar@gmail.com>
2024-06-14 18:14:35 -0400

    deploy infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 resource "aws_instance" "server" {
   ami = "ami-04b70fa74e45c3917"
-  instance_type = "t2-micro"
+  instance_type = "t2.micro"
   key_name = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
   iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
